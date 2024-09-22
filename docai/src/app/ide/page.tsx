0862b82b0d9428b305b554145dad1b6bc9fa424a
Isuru Akalanka <it21305900@my.sliit.lk>
2024-09-22 22:10:00 +0530

    app initiate

---
+++ docai/src/app/ide/page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const page = async ({
+  searchParams,
+}: {
+  searchParams?: {
+    repository?: string;
+  };
+}) => {
+  const { repository }: any = await searchParams;
+
+  return (
+    <div className="bg-black text-slate-200">
+      <div className="flex flex-col h-[90vh] justify-center items-center space-y-2">
+        <h1>Select a Repository</h1>
+        <div className="min-w-[300px]"></div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
