bf7249f7e68a4d398b475c9c095061813b70c517
Isuru Akalanka <it21305900@my.sliit.lk>
2024-09-23 21:20:40 +0530

    route create

--- docai/src/app/ide/page.tsx
+++ docai/src/app/ide/page.tsx
@@ -12,7 +12,6 @@ const page = async ({
   return (
     <div className="bg-black text-slate-200">
       <div className="flex flex-col h-[90vh] justify-center items-center space-y-2">
-        <h1>Select a Repository</h1>
         <div className="min-w-[300px]"></div>
       </div>
     </div>
