75f964efc7739e134b7f7c62eee80a287bb27c70
Isuru Akalanka <it21305900@my.sliit.lk>
2024-10-22 19:40:00 +0530

    project selector and code file selector

--- docai/src/app/ide/page.tsx
+++ docai/src/app/ide/page.tsx
@@ -1,3 +1,8 @@
+import RepositorySelector from "@/components/common/RepositorySelector";
+import IDE from "@/components/features/ide/ide";
+import { getFileIcon } from "@/core/config/file-icons";
+import { FileExplorer, projectStructure } from "@/core/config/tree-object";
+import { buildProjectStructure } from "@/lib/filesystem";
 import React from "react";
 
 const page = async ({
@@ -9,13 +14,28 @@ const page = async ({
 }) => {
   const { repository }: any = await searchParams;
 
-  return (
-    <div className="bg-black text-slate-200">
-      <div className="flex flex-col h-[90vh] justify-center items-center space-y-2">
-        <div className="min-w-[300px]"></div>
+  if (!repository)
+    return (
+      <div className="bg-black text-slate-200">
+        <div className="flex flex-col h-[90vh] justify-center items-center space-y-2">
+          <h1>Select a Repository</h1>
+          <div className="min-w-[300px]">
+            <RepositorySelector />
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  else {
+    const structure = await buildProjectStructure(
+      `./repositories/${repository}`
+    );
+
+    return (
+      <div>
+        <IDE root={structure} theme="dark" customIcons={getFileIcon} />
+      </div>
+    );
+  }
 };
 
 export default page;
