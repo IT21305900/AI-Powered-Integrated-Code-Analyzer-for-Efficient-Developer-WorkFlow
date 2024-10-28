27cb5d4cd6893bd0e6149f271e7750164188d8e5
Isuru Akalanka <it21305900@my.sliit.lk>
2024-10-28 21:40:00 +0530

    pipeline initializer

---
+++ docai/src/components/pipeline/documentation/DocumentPipeline.tsx
@@ -0,0 +1,62 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { PlayCircle } from "lucide-react";
+import { useSearchParams } from "next/navigation";
+import React, { ReactNode } from "react";
+import { embedAllJsonFiles } from "../../../../../scripts/vectorize";
+import { analyzeAndBuildGraph } from "../../../../../scripts/analyzer";
+import { generateDocumentation } from "../../../../../scripts/generator";
+import { callTools } from "../../../../../scripts/tools";
+
+const DocumentPipeline = () => {
+  const searchParams = useSearchParams();
+
+  const repository = searchParams.get("repository");
+
+  return (
+    <div className="space-y-4 overflow-y-auto">
+      <div className="border-b py-1">
+        <p className="lead mx-5">Documentation Pipeline</p>
+      </div>
+
+      <div className="space-y-3 mx-5">
+        <Step step="Select the project" index={1}>
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label htmlFor="repo">Project</Label>
+            <Input id="repo" value={repository!} disabled />
+          </div>
+        </Step>
+      </div>
+    </div>
+  );
+};
+
+export default DocumentPipeline;
+
+const Step = ({
+  children,
+  step,
+  index,
+}: {
+  children: ReactNode;
+  step: string;
+  index: number;
+}) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardDescription>Step {index}</CardDescription>
+        <CardTitle className="font-medium">{step}</CardTitle>
+      </CardHeader>
+      <CardContent>{children}</CardContent>
+    </Card>
+  );
+};
