03b2881ef90eb37bf4c039ac291397ad3360b29b
Isuru Akalanka <hostakalanka@gmail.com>
2025-05-23 14:04:22 +0530

    Ui update

--- docai/src/components/features/documentation/MarkdownEditor.tsx
+++ docai/src/components/features/documentation/MarkdownEditor.tsx
@@ -136,10 +136,12 @@ const Editor = () => {
                 remarkPlugins={[remarkGfm]}
                 rehypePlugins={[rehypeRaw]}
                 components={{
+                  // @ts-ignore
                   code({ node, inline, className, children, ...props }) {
                     const match = /language-(\w+)/.exec(className || "");
                     return !inline && match ? (
                       <SyntaxHighlighter
+                        // @ts-ignore
                         style={atomDark}
                         language={match[1]}
                         PreTag="div"
