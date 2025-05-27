20b830e4bc4ddad2b447eae9a5c019c9a344cc58
Isuru Akalanka <hostakalanka@gmail.com>
2025-05-27 15:41:23 +0530

    pipline fixed

--- docai/src/app/api/pipeline/[repository]/route.ts
+++ docai/src/app/api/pipeline/[repository]/route.ts
@@ -1,20 +1,20 @@
-// import { getPipelineStats } from "@/lib/actions/documentstats.action";
+import { getPipelineStats } from "@/lib/actions/documentstats.action";
 
-// export const GET = async (
-//     request: Request,
-//     { params }: { params: { repository: string } }
-// ) => {
-//     console.log("Called")
-//     const stats = await getPipelineStats(params.repository);
+export const GET = async (
+    request: Request,
+    { params }: { params: { repository: string } }
+) => {
+    console.log("Called")
+    const stats = await getPipelineStats(params.repository);
 
-//     return Response.json(stats || {});
-// };
+    return Response.json(stats || {});
+};
 
 
-import type { NextApiRequest, NextApiResponse } from 'next'
+// import type { NextApiRequest, NextApiResponse } from 'next'
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-    console.log(req.query)
+// export async function GET(req: NextApiRequest, res: NextApiResponse) {
+//     console.log(req.query)
 
-    return Response.json( {});
-}
+//     return Response.json({});
+// }
